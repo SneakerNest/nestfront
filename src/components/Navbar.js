bfe5324334ef235194eadf3b75e72bbd6e0a6612
mertk <mertk.aycan@gmail.com>
2025-03-28 16:36:38 +0300

    made sneaker nest clıckable and ımprovements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; // <-- Important if using React Router
 import "../styles/Navbar.css";
-import logo from "../assets/2.png"; 
+import logo from "../assets/2.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faUser, faHeart, faShoppingCart, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faUser,
+  faHeart,
+  faShoppingCart,
+  faBars,
+  faTimes
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -20,19 +28,23 @@ const Navbar = () => {
   return (
     <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
       <div className="nav-left">
-        <FontAwesomeIcon 
-          icon={isMenuOpen ? faTimes : faBars} 
-          className="menu-icon" 
-          onClick={() => setIsMenuOpen(!isMenuOpen)} 
+        <FontAwesomeIcon
+          icon={isMenuOpen ? faTimes : faBars}
+          className="menu-icon"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
         />
-        <img src={logo} alt="SneakerNest Logo" className="nav-logo" />
+        
+        {/* Wrap the logo in a Link so it navigates to the homepage on click */}
+        <Link to="/">
+          <img src={logo} alt="SneakerNest Logo" className="nav-logo" />
+        </Link>
       </div>
 
       <ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
-        <li><a href="/sneakers">Sneakers</a></li>
-        <li><a href="/casual">Casual</a></li>
-        <li><a href="/boots">Boots</a></li>
-        <li><a href="/slippers">Slippers & Sandals</a></li>
+        <li><Link to="/sneakers">Sneakers</Link></li>
+        <li><Link to="/casual">Casual</Link></li>
+        <li><Link to="/boots">Boots</Link></li>
+        <li><Link to="/slippers">Slippers & Sandals</Link></li>
       </ul>
 
       <div className="nav-icons">
